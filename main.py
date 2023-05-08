e0563847a7c628e41931e626dd83110111d512b1
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2023-05-08 22:25:01 +0900

    fix: rename

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ current_dir = os.getcwd()
 now = datetime.now()
 yesterday = now - timedelta(1)
 
-yesterday_report_name = yesterday.strftime("%m_%d_%Y_report.md")
-new_report_name = now.strftime("%m_%d_%Y_report.md")
-yesterday_report_path = os.path.join(current_dir, yesterday_report_name)
-new_report_path = os.path.join(current_dir, new_report_name)
+yesterday_log_name = yesterday.strftime("%m_%d_%Y_log.md")
+new_log_name = now.strftime("%m_%d_%Y_log.md")
+yesterday_log_path = os.path.join(current_dir, yesterday_log_name)
+new_log_path = os.path.join(current_dir, new_log_name)
 
-def create_new_report():
-    with open(new_report_path, "w") as f:
-        f.write(now.strftime("# %Y/%m/%d Daily Report"))
+def create_new_log():
+    with open(new_log_path, "w") as f:
+        f.write(now.strftime("# %Y/%m/%d Daily log"))
         f.write("\n")
         f.write("\n")
         f.write("## TODO List")
@@ -31,13 +31,13 @@ def create_new_report():
         f.write("## +αで達成できたら良いこと")
         f.write("\n")
 
-def copy_yesterdays_report(src_file_path, dest_file_path):
+def copy_yesterdays_log(src_file_path, dest_file_path):
     shutil.copy(src_file_path, dest_file_path)
 
 def check_file_exists(file_path):
     return os.path.exists(file_path)
 
-if check_file_exists(yesterday_report_path):
-    copy_yesterdays_report(yesterday_report_path, new_report_path)
+if check_file_exists(yesterday_log_path):
+    copy_yesterdays_log(yesterday_log_path, new_log_path)
 else:
-    create_new_report()
+    create_new_log()
